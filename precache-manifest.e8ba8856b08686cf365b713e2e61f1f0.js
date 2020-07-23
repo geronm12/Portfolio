95eb393a9d67b11fbe5a82710aab1c2ff22ee522
geronimo <gerolpz01@gmail.com>
2020-07-22 22:45:28 -0300

    Updates

---
+++ precache-manifest.e8ba8856b08686cf365b713e2e61f1f0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8417411136d5fb0fc5e279c593a3610",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "32266e58e8ab3030a216",
+    "url": "/Portfolio/static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "482fb9b7cbf0c78d31aa",
+    "url": "/Portfolio/static/css/main.0882281c.chunk.css"
+  },
+  {
+    "revision": "32266e58e8ab3030a216",
+    "url": "/Portfolio/static/js/2.46764c18.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio/static/js/2.46764c18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "482fb9b7cbf0c78d31aa",
+    "url": "/Portfolio/static/js/main.7ea84657.chunk.js"
+  },
+  {
+    "revision": "c66aa5eafc120060b1dc",
+    "url": "/Portfolio/static/js/runtime-main.efb1fe8d.js"
+  },
+  {
+    "revision": "00387cbb67338a3391446c4517074cd2",
+    "url": "/Portfolio/static/media/NET.00387cbb.png"
+  },
+  {
+    "revision": "78ab6075ca208a3a606ffa0a12a4aec0",
+    "url": "/Portfolio/static/media/background.78ab6075.jpg"
+  },
+  {
+    "revision": "cef8fe028b3ff90071c5b17b771e9862",
+    "url": "/Portfolio/static/media/cscss.cef8fe02.png"
+  },
+  {
+    "revision": "98a94f45afdb993edf4fe70d75295d35",
+    "url": "/Portfolio/static/media/flutter-dart.98a94f45.png"
+  },
+  {
+    "revision": "c248cd3f4baab4eb842fdc591782cb40",
+    "url": "/Portfolio/static/media/golang.c248cd3f.jpeg"
+  },
+  {
+    "revision": "69178c7d641f57983c37f9ca6b2c5151",
+    "url": "/Portfolio/static/media/html.69178c7d.jpg"
+  },
+  {
+    "revision": "7b99454981f0895af07527183253a053",
+    "url": "/Portfolio/static/media/react.7b994549.png"
+  },
+  {
+    "revision": "2859afc1cabf4cc04fd231ba3cb8512e",
+    "url": "/Portfolio/static/media/scss.2859afc1.jpg"
+  }
+]);
