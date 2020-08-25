1ec75f0fcea399a836e103aef083db0363d24632
geronimo <gerolpz01@gmail.com>
2020-08-25 10:48:12 -0300

    ...

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -19,21 +19,21 @@ export default function AboutPage(props) {
             </div>
             </div>
             <p>
-                "Aquél que puede conquista a los enemigos es fuerte. Aquel que se ha conquistado a sí mismo es poderoso"
-                 Con está frase de Lao Tse me presento.  Mi nombre es Gerónimo Naum López y hago lo que amo... 
-                 Que bonito poder decir eso ¿no?. </p>
+                "Aquél que puede conquista a los enemigos es fuerte. Aquél que se ha conquistado a sí mismo es poderoso"
+                 Con esta frase de Lao Tse me presento. Mi nombre es Gerónimo Naum López y hago lo que amo... 
+                 ¡Qué bonito poder decir eso! ¿no?. </p>
                    <p>Soy software developer, una profesión que me crucé por pura suerte, por casualidades o quizás
                  causalidades ¿quién sabe?, al día de hoy el debate entre la existencia o no del destino (y tantas cosas) debería dejar de ser el foco, y deberíamos
                  concentrarnos en el creer, una vez leí una frase que decía "Creo que cuando creo, creo". ¿Qué loco, no? </p>
 
-                  <p>    Comencé en el año 2018 en la carrera Técnico Univeristario en Programación y al día de la fecha puedo decir 
+                  <p>  Comencé en el año 2018 en la carrera Técnico Univeristario en Programación y al día de la fecha puedo decir 
                  que tengo la dicha de hacer algo que realmente me apasiona, algo que me mantiene vivo y me motiva a querer crecer, aprender
                  constantemente. </p>
                  <p>Creo que el Software y la programación pueden cambiar la vida de las personas. Estoy en un mundo muy basto,
                  con muchísimo para dar y un conocimiento completamente infinito y nuevo. </p>
                  <p>Un mundo donde cada día es un nuevo desafío y donde
                  todos pueden aportar. Donde trabajamos en conjunto, en equipo, para poder crear algo que va más allá de las capacidades, y ¡que sabio
-                 fue Walt Disney! al decir "Los grandes logros de cualquier persona generalmente dependen de muchas manos, corazones y mentes".</p>
+                 fue Walt Disney! al decir "Los grandes logros de cualquier persona generalmente dependen de muchas manos, corazónes y mentes".</p>
                  <p>Bienvenidos a mi Portfolio, página que intentaré mantener actualizada constantemente con mis proyectos, conocimientos, experiencias y pensamientos.
                   Muchas gracias por darte una vuelta. Las puertas de mi casa siempre estarán abiertas.
                         </p>
