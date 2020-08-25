5cb767d8dc49fe7ffdc1f6bd24ca9b406478eec0
geronimo <gerolpz01@gmail.com>
2020-08-25 18:55:18 -0300

    Agregado modal, falta mejorar algunas cosas del css

--- src/pages/Quotes/Quotes.js
+++ src/pages/Quotes/Quotes.js
@@ -1,12 +1,23 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React from 'react';
+import React, {useState} from 'react';
 import {Tecnologias} from '../tecnologias';
+import { Image, Modal, Button } from 'semantic-ui-react';
+
 
 import "./Quotes.scss";
 
 export default function Quotes(props) {
 
     const {id} = props;
+
+    const [open, setOpen] = useState(false);
+    const [tecnologia, setTecnologia] = useState(Tecnologias[0]);
+
+    const handleModal = (tec) => {
+        setTecnologia(tec);
+        setOpen(true);
+    }
+
 
     return (
         
@@ -16,16 +27,17 @@ export default function Quotes(props) {
             <div className="quote-page__container">
             {Tecnologias.map((tec, index) => 
              
-             <div className="quote-page__container__card" key={index}>
-            
+             <div className="quote-page__container__card" key={index} onClick={() => handleModal(tec)}>
              <img style={{backgroundImage: `url('${tec.img}')`}}  className="quote-page__container__card__item"/>
              <div className="quote-page__container__card__texto">
              <h2>{tec.nombre}</h2>
              <p>{tec.text.substring(0, 80) + "..."}</p>
              </div>
              </div>
-
             )}
+              
+
+            <TecnologiaModal open={open} setOpen={setOpen} tecnologia={tecnologia}/>
           </div>
             
          
@@ -33,4 +45,29 @@ export default function Quotes(props) {
      
         </div>
     )
+}
+
+
+function TecnologiaModal (props) {
+
+    const {setOpen, open, tecnologia} = props;
+ 
+    return (
+    <Modal onClose={() => setOpen(false)} open={open} className="modal">
+     <Modal.Header className="modal__title">{tecnologia.nombre}</Modal.Header>
+      <Modal.Content  className="modal__content">
+        <div style={{backgroundImage: `url('${tecnologia.img}')`}} className="modal__content__image"></div>
+        <Modal.Description>
+         <div className="modal__content__description">
+            <div className="modal__content__description__p">
+                <p>
+                    {tecnologia.text}
+                </p>
+            </div>
+           </div>
+        
+        </Modal.Description>
+          </Modal.Content>
+    </Modal>
+);
 }
